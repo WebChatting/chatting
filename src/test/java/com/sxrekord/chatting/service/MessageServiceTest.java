7332b22d07d8f560f7955207ca2abc8c3f34157b
Rekord <1324596506@qq.com>
2022-09-13 22:50:35 +0800

    test: perf test cases

--- src/test/java/com/sxrekord/chatting/service/MessageServiceTest.java
+++ src/test/java/com/sxrekord/chatting/service/MessageServiceTest.java
@@ -1,10 +1,15 @@
 package com.sxrekord.chatting.service;
 
+import com.sxrekord.chatting.dao.FileContentDao;
 import com.sxrekord.chatting.dao.MessageDao;
+import com.sxrekord.chatting.dao.TextContentDao;
+import com.sxrekord.chatting.model.po.FileContent;
 import com.sxrekord.chatting.model.po.Message;
+import com.sxrekord.chatting.model.po.TextContent;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 /**
  * @author Rekord
@@ -13,6 +18,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class MessageServiceTest {
 
+    @Autowired
+    FileContentDao fileContentDao;
+    @Autowired
+    TextContentDao textContentDao;
     @Autowired
     MessageDao messageDao;
 
@@ -23,6 +32,17 @@ public class MessageServiceTest {
 
     @Test
     public void insertMessageTest() {
-        System.out.println(messageDao.insertMessage(new Message(501L, 503L, 0, 0, 1003L)));
+        int feedback = 0;
+        TextContent textContent = new TextContent("text message");
+        textContentDao.insertTextContent(textContent);
+        feedback = messageDao.insertMessage(new Message(501L, 503L, 0, 0, textContent.getId()));
+        Assert.isTrue(feedback == 1, "insert text message error!");
+
+        FileContent fileContent = new FileContent("test.pdf", "322KB", "/UploadFile/b88cbb8786604ea6bcba0be61743de5e.pdf");
+        fileContentDao.insertFileContent(fileContent);
+        feedback = messageDao.insertMessage(new Message(501L, 503L, 0, 1, fileContent.getId()));
+        Assert.isTrue(feedback == 1, "insert file message error");
+
+        System.out.println("insert message success");
     }
 }
