7989b32a20a5495fb135ae779109c9b1dfe8d805
Rekord <1324596506@qq.com>
2022-09-13 16:56:31 +0800

    chore: update data.sql

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -29,6 +29,26 @@ insert into relation (`request_id`, `accept_id`, `type`) values (503, 504, 0);
 insert into relation (`request_id`, `accept_id`, `type`) values (503, 505, 0);
 insert into relation (`request_id`, `accept_id`, `type`) values (504, 505, 0);
 
+-- insert text message
+insert into text_content (`content`) values ('hi');
+insert into text_content (`content`) values ('hello');
+insert into text_content (`content`) values ("I'm rekord");
+insert into text_content (`content`) values ("I'm KangKang");
+
+insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (501, 502, 0, 0, 1001);
+insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (501, 502, 0, 0, 1002);
+insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (501, 502, 0, 0, 1003);
+insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (501, 502, 0, 0, 1004);
+
+
+-- insert file message
+insert into file_content (`name`, `size`, `path`) values ('中文成绩单.pdf', '165.6KB', '/UploadFile/b88cbb8786604ea6bcba0be61743de5e.pdf');
+insert into file_content (`name`, `size`, `path`) values ('英文成绩单.pdf', '165.6KB', '/UploadFile/b88cbb8786604ea6bcba0be61743de5e.pdf');
+
+insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (501, 502, 0, 1, 1001);
+insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (501, 502, 0, 1, 1002);
+
+
 
 
 
