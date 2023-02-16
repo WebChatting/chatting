629963f32220a5e9218c6a5971ebaf25b36a41b7
Rekord <1324596506@qq.com>
2023-02-16 17:10:56 +0800

    refactor: refactor chatting database

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -7,27 +7,26 @@ insert into user (`username`, `password`, `avatar_path`) values ('member003', '0
 insert into user (`username`, `password`, `avatar_path`) values ('member004', '004', 'avatar/default_user_avatar.jpg');
 insert into user (`username`, `password`, `avatar_path`) values ('member005', '005', 'avatar/default_user_avatar.jpg');
 
--- init group
+-- create group
 -- 1. create group (the group is a keyword)
-insert into `group` (`group_name`, `avatar_path`, `owner`) values ('group001', 'avatar/default_group_avatar.jpg', 501);
+insert into `group` (`name`, `owner_id`, `avatar_path`) values ('group001', 501, 'avatar/default_group_avatar.jpg');
 -- 2. add user to group
-insert into relation (`request_id`, `accept_id`, `type`) values (501, 101, 1);
-insert into relation (`request_id`, `accept_id`, `type`) values (502, 101, 1);
-insert into relation (`request_id`, `accept_id`, `type`) values (503, 101, 1);
-insert into relation (`request_id`, `accept_id`, `type`) values (504, 101, 1);
-insert into relation (`request_id`, `accept_id`, `type`) values (505, 101, 1);
+insert into relation (`request_id`, `accept_id`, `type`, `status`) values (502, 501, 1, 0);
+insert into relation (`request_id`, `accept_id`, `type`, `status`) values (503, 501, 1, 1);
+insert into relation (`request_id`, `accept_id`, `type`, `status`) values (504, 501, 1, 2);
+insert into relation (`request_id`, `accept_id`, `type`, `status`) values (505, 501, 1, 3);
 
 -- build user relation
-insert into relation (`request_id`, `accept_id`, `type`) values (501, 502, 0);
-insert into relation (`request_id`, `accept_id`, `type`) values (501, 503, 0);
-insert into relation (`request_id`, `accept_id`, `type`) values (501, 504, 0);
-insert into relation (`request_id`, `accept_id`, `type`) values (501, 505, 0);
-insert into relation (`request_id`, `accept_id`, `type`) values (502, 503, 0);
-insert into relation (`request_id`, `accept_id`, `type`) values (502, 504, 0);
-insert into relation (`request_id`, `accept_id`, `type`) values (502, 505, 0);
-insert into relation (`request_id`, `accept_id`, `type`) values (503, 504, 0);
-insert into relation (`request_id`, `accept_id`, `type`) values (503, 505, 0);
-insert into relation (`request_id`, `accept_id`, `type`) values (504, 505, 0);
+insert into relation (`request_id`, `accept_id`, `type`, `status`) values (501, 502, 0, 0);
+insert into relation (`request_id`, `accept_id`, `type`, `status`) values (501, 503, 0, 1);
+insert into relation (`request_id`, `accept_id`, `type`, `status`) values (501, 504, 0, 2);
+insert into relation (`request_id`, `accept_id`, `type`, `status`) values (501, 505, 0, 3);
+insert into relation (`request_id`, `accept_id`, `type`, `status`) values (502, 503, 0, 1);
+insert into relation (`request_id`, `accept_id`, `type`, `status`) values (502, 504, 0, 2);
+insert into relation (`request_id`, `accept_id`, `type`, `status`) values (502, 505, 0, 3);
+insert into relation (`request_id`, `accept_id`, `type`, `status`) values (503, 504, 0, 0);
+insert into relation (`request_id`, `accept_id`, `type`, `status`) values (503, 505, 0, 3);
+insert into relation (`request_id`, `accept_id`, `type`, `status`) values (504, 505, 0, 1);
 
 -- insert text message
 insert into text_content (`content`) values ('hi');
@@ -37,28 +36,15 @@ insert into text_content (`content`) values ("I'm KangKang");
 insert into text_content (`content`) values ("hi, I'm member001");
 insert into text_content (`content`) values ("hi, I'm member002");
 
-insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (501, 502, 0, 0, 1001);
-insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (502, 501, 0, 0, 1002);
-insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (501, 502, 0, 0, 1003);
-insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (502, 501, 0, 0, 1004);
+insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (501, 503, 0, 0, 1001);
+insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (503, 501, 0, 0, 1002);
+insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (501, 503, 0, 0, 1003);
+insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (503, 501, 0, 0, 1004);
 insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (501, 101, 1, 0, 1005);
-insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (502, 101, 1, 0, 1006);
+insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (503, 101, 1, 0, 1006);
 
+
+-- insert image message
 
 -- insert file message
-insert into file_content (`name`, `size`, `path`) values ('中文成绩单.pdf', '165.6KB', 'UploadFile/b88cbb8786604ea6bcba0be61743de5e.pdf');
-insert into file_content (`name`, `size`, `path`) values ('英文成绩单.pdf', '165.6KB', 'UploadFile/b88cbb8786604ea6bcba0be61743de5e.pdf');
-
-insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (501, 502, 0, 1, 1001);
-insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (502, 501, 0, 1, 1002);
-insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (501, 101, 1, 1, 1001);
-insert into message (`from_id`, `to_id`, `type`, `content_type`, `content_id`) values (502, 101, 1, 1, 1002);
-
-
-
-
-
-
-
-
 
--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -3,64 +3,87 @@ set foreign_key_checks = 0;
 -- create chatting database
 drop database if exists chatting;
 create database chatting;
--- enter chatting 
+-- enter chatting
 use chatting;
 
 -- create user table
 create table if not exists `user` (
-    `user_id` bigint unsigned not null auto_increment comment '用户ID',
-    `username` varchar(20) unique not null comment '用户账号',
+    `id` bigint unsigned not null auto_increment comment '用户ID',
+    `username` varchar(16) unique not null comment '用户账号',
     `password` varchar(16) not null comment '用户密码',
     `avatar_path` varchar(255) not null comment '用户头像存储路径',
-    primary key (`user_id`) using btree,
-    unique index (`username`) using btree
-) ENGINE=InnoDB auto_increment=501 DEFAULT charset=utf8;
-
--- create message table
-create table if not exists `message` (
-	`message_id` bigint unsigned not null auto_increment comment '消息ID',
-    `from_id` bigint unsigned not null comment '发送方用户ID',
-    `to_id` bigint unsigned not null comment '接收方用户ID',
-    `type` int not null comment '消息类型',
-    `content_type` int not null comment '消息内容类型',
-    `content_id` bigint unsigned not null comment '消息ID',
-    `send_time` timestamp not null default current_timestamp comment '消息发送时间',
-    primary key (`message_id`) using btree,
-    constraint FK_MESSAGE_FROM_ID foreign key(`from_id`) references user(`user_id`) on delete cascade
-) ENGINE=InnoDB DEFAULT charset=utf8;
+    `create_time` timestamp default current_timestamp comment '创建时间',
+    `update_time` timestamp default current_timestamp on update current_timestamp comment '更新时间',
+    primary key `pk_id` (`id`),
+    unique key `uk_username` (`username`)
+) ENGINE=InnoDB auto_increment=501 DEFAULT charset=utf8mb4;
 
 -- create group table
 create table if not exists `group` (
-	`group_id` bigint unsigned not null auto_increment comment '群组ID',
-    `group_name` varchar(20) not null comment '群组名',
-    `avatar_path` varchar(266) not null comment '群组头像存储路径',
-    `owner` bigint unsigned not null comment '创建者ID',
-    primary key (`group_id`) using btree,
-    constraint FK_GROUP_OWNER foreign key(`owner`) references user(`user_id`) on delete cascade
-) ENGINE=InnoDB auto_increment=101 DEFAULT charset=utf8;
+	`id` bigint unsigned not null auto_increment comment '群组ID',
+    `name` varchar(16) unique not null comment '群组名',
+    `owner_id` bigint unsigned not null comment '创建者ID',
+    `avatar_path` varchar(255) not null comment '群组头像存储路径',
+    `create_time` timestamp default current_timestamp comment '创建时间',
+    `update_time` timestamp default current_timestamp on update current_timestamp comment '更新时间',
+    primary key `pk_id` (`id`),
+    unique key `uk_name` (`name`),
+    constraint `fk_owner_id` foreign key (`owner_id`) references `user` (`id`) on delete cascade
+) ENGINE=InnoDB auto_increment=101 DEFAULT charset=utf8mb4;
+
+-- create message table
+create table if not exists `message` (
+	`id` bigint unsigned not null auto_increment comment '消息ID',
+    `from_id` bigint unsigned not null comment '发送方用户ID',
+    `to_id` bigint unsigned not null comment '接收方用户ID',
+    `type` tinyint unsigned not null comment '消息类型(0 ~ 255)，私聊(0)/群聊(1)消息',
+    `content_type` tinyint unsigned not null comment '消息内容类型，文本(0)/图片(1)/文件(2)',
+    `content_id` bigint unsigned not null comment '消息内容ID',
+    `create_time` timestamp default current_timestamp comment '消息发送时间',
+    `update_time` timestamp default current_timestamp on update current_timestamp comment '更新时间',
+    primary key `pk_id` (`id`),
+    constraint `fk_from_id` foreign key (`from_id`) references `user` (`id`) on delete cascade
+) ENGINE=InnoDB DEFAULT charset=utf8mb4;
+
 
 -- create relation table
 create table if not exists `relation` (
 	`id` bigint unsigned not null auto_increment comment '关系ID',
     `request_id` bigint unsigned not null comment '建立关系时的请求方用户ID',
     `accept_id` bigint unsigned not null comment '建立关系时的接受方用户ID和群组ID',
-    `type` int not null comment '关系类型，好友/群友 关系',
-    primary key (`id`) using btree,
-    constraint FK_RELATION_REQUEST_ID foreign key(`request_id`) references user(`user_id`) on delete cascade
-) ENGINE=InnoDB DEFAULT charset=utf8;
+    `type` tinyint unsigned not null comment '关系类型，好友(0)/群友(1) 关系',
+    `status` tinyint unsigned not null comment '关系状态，等待建立关系(0)/已建立关系(1)/拒绝建立关系(2)/删除已建立关系(3)',
+    `create_time` timestamp default current_timestamp comment '创建时间',
+    `update_time` timestamp default current_timestamp on update current_timestamp comment '更新时间',
+    primary key `pk_id` (`id`),
+    constraint `fk_request_id` foreign key (`request_id`) references `user` (`id`) on delete cascade,
+    constraint `fk_accept_id` foreign key (`accept_id`) references `user` (`id`) on delete cascade
+) ENGINE=InnoDB default charset=utf8mb4;
 
 create table if not exists `text_content` (
 	`id` bigint unsigned not null auto_increment comment '文本消息ID',
     `content` varchar(1024) not null comment '文本消息',
-    primary key (`id`) using btree
-) engine=InnoDB auto_increment=1001 default charset=utf8;
+    `create_time` timestamp default current_timestamp comment '创建时间',
+    `update_time` timestamp default current_timestamp on update current_timestamp comment '更新时间',
+    primary key `pk_id` (`id`)
+) engine=InnoDB auto_increment=1001 default charset=utf8mb4;
 
 create table if not exists `file_content` (
 	`id` bigint unsigned not null auto_increment comment '文本消息ID',
     `name` varchar(255) not null comment '文件名',
     `size` varchar(12) not null comment '文件大小（带单位）',
     `path` varchar(255) not null comment '文件存储路径',
-    primary key (`id`) using btree
-) engine=InnoDB auto_increment=1001 default charset=utf8;
+    `create_time` timestamp default current_timestamp comment '创建时间',
+    `update_time` timestamp default current_timestamp on update current_timestamp comment '更新时间',
+    primary key `pk_id` (`id`)
+) engine=InnoDB auto_increment=1001 default charset=utf8mb4;
+
+create table if not exists `image_content` (
+	`id` bigint unsigned not null auto_increment comment '图片消息ID',
+    `path` varchar(255) not null comment '图片存储路径',
+    `create_time` timestamp default current_timestamp comment '创建时间',
+    `update_time` timestamp default current_timestamp on update current_timestamp comment '更新时间',
+    primary key `pk_id` (`id`)
+) engine=InnoDB auto_increment=1001 default charset=utf8mb4;
 
 set foreign_key_checks = 1;
