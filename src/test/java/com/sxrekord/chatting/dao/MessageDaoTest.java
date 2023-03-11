56c93473071f23647e7cea77e784cae68133316c
Rekord <1324596506@qq.com>
2023-03-11 20:14:39 +0800

    test: add test for messageDao.listMessage

--- src/test/java/com/sxrekord/chatting/dao/MessageDaoTest.java
+++ src/test/java/com/sxrekord/chatting/dao/MessageDaoTest.java
@@ -6,8 +6,11 @@ import com.sxrekord.chatting.model.po.TextContent;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.util.Assert;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -45,5 +48,29 @@ public class MessageDaoTest {
         Assert.isTrue(feedback == 1, "insert file message error");
 
         System.out.println("insert message success");
+    }
+
+    @Test
+    public void listMessageTest() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.DATE,1);
+
+        List<Message> feedback = null;
+        // 列出2条私聊消息
+        System.out.println(feedback = messageDao.listMessage(0, calendar.getTime(), 501L, 503L, 2));
+        Assert.isTrue(feedback.size() == 2, "list message error");
+        // 列出大量私聊消息
+        System.out.println(feedback = messageDao.listMessage(0, calendar.getTime(), 501L, 503L, Integer.MAX_VALUE));
+        Assert.isTrue(feedback.size() == 4, "list message error");
+
+        // 列出2条群聊消息
+        System.out.println(feedback = messageDao.listMessage(1, calendar.getTime(), 501L, 101L, 2));
+        Assert.isTrue(feedback.size() == 1, "list message error");
+        // 列出大量群聊消息
+        System.out.println(feedback = messageDao.listMessage(1, calendar.getTime(), 503L, 101L, Integer.MAX_VALUE));
+        Assert.isTrue(feedback.size() == 1, "list message error");
+
+        System.out.println("list message success");
     }
 }
