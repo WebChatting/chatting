4c62465e230cd1b902dec210331e2e2e429afb5f
Rekord <1324596506@qq.com>
2023-03-11 21:08:07 +0800

    test: add test for messageService.loadMessage

--- src/test/java/com/sxrekord/chatting/service/MessageServiceTest.java
+++ src/test/java/com/sxrekord/chatting/service/MessageServiceTest.java
@@ -4,7 +4,12 @@ import com.sxrekord.chatting.model.vo.ResponseJson;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.util.Assert;
+
+import javax.servlet.http.HttpSession;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * @author Rekord
@@ -28,5 +33,24 @@ public class MessageServiceTest {
         ResponseJson responseJson = messageService.getGroupMessage(101L);
         Assert.isTrue(responseJson != null, "get groupMessage fail");
         System.out.println(responseJson);
+    }
+
+    @Test
+    public void loadMessageTest() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.DATE,1);
+
+        HttpSession httpSession = new MockHttpSession();
+        httpSession.setAttribute("userId", 501L);
+
+        System.out.println(messageService.loadMessage(0, calendar.getTime(), 503L, 2, httpSession));
+
+        System.out.println(messageService.loadMessage(0, calendar.getTime(), 503L, Integer.MAX_VALUE, httpSession));
+
+        System.out.println(messageService.loadMessage(1, calendar.getTime(), 101L, 2, httpSession));
+
+        httpSession.setAttribute("userId", 503L);
+        System.out.println(messageService.loadMessage(1, calendar.getTime(), 101L, Integer.MAX_VALUE, httpSession));
     }
 }
