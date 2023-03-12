e95f87d0fc4e8219fa30044e368c1e3a1436ed92
Rekord <1324596506@qq.com>
2023-03-12 13:29:44 +0800

    test: add test for groupDao.searchGroupByName

---
+++ src/test/java/com/sxrekord/chatting/dao/GroupDaoTest.java
@@ -0,0 +1,28 @@
+package com.sxrekord.chatting.dao;
+
+import com.sxrekord.chatting.model.po.Group;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+/**
+ * @author Rekord
+ * @date 2023/3/11 21:39
+ */
+@SpringBootTest
+public class GroupDaoTest {
+    @Autowired
+    GroupDao groupDao;
+
+    @Test
+    public void testSearchGroupByName() {
+        List<Group> feedback = null;
+        System.out.println(feedback = groupDao.searchGroupByName("group"));
+        Assert.isTrue(feedback != null, "test searchGroupByName error");
+        System.out.println("test searchGroupByName success");
+    }
+
+}
