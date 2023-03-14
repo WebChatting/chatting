bf1cf5364721201f578cc824086f01abd288d17a
Rekord <1324596506@qq.com>
2023-03-14 14:28:50 +0800

    chore: offer userId after login

--- src/main/java/com/sxrekord/chatting/service/impl/UserServiceImpl.java
+++ src/main/java/com/sxrekord/chatting/service/impl/UserServiceImpl.java
@@ -53,7 +53,8 @@ public class UserServiceImpl implements UserService {
         if (user == null) {
             responseJson.error("用户名或密码错误！");
         } else {
-            responseJson.setData("username", user.getUsername())
+            responseJson.setData("id", user.getId())
+                    .setData("username", user.getUsername())
                     .setData("password", user.getPassword())
                     .setData("avatarPath", user.getAvatarPath()).success();
             if (session != null) {
