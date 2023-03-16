77e03211171988feb93c09c5f93b5e6701a70b53
Rekord <1324596506@qq.com>
2023-03-16 22:17:16 +0800

    fix: fix the return contentType error

---
+++ src/main/java/com/sxrekord/chatting/dao/ImageContentDao.java
@@ -0,0 +1,19 @@
+package com.sxrekord.chatting.dao;
+
+import com.sxrekord.chatting.model.po.ImageContent;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author Rekord
+ * @date 2023/3/16 19:07
+ */
+@Mapper
+public interface ImageContentDao {
+    /**
+     * 根据id查询ImageContent
+     * @param id
+     * @return
+     */
+    ImageContent getImageContentById(@Param("id") Long id);
+}
