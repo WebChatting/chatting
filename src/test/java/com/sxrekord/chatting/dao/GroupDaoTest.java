f7bba13fa00dd592071f93aad1f3a52eb21cf9f8
Rekord <1324596506@qq.com>
2023-03-31 23:30:16 +0800

    feat: offer pagehelper

--- src/test/java/com/sxrekord/chatting/dao/GroupDaoTest.java
+++ src/test/java/com/sxrekord/chatting/dao/GroupDaoTest.java
@@ -1,5 +1,8 @@
 package com.sxrekord.chatting.dao;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.sxrekord.chatting.model.po.Group;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,20 @@ import java.util.List;
 public class GroupDaoTest {
     @Autowired
     GroupDao groupDao;
+
+    @Test
+    public void testPageHelperForSearchGroupByName() {
+        PageHelper.startPage(-1, 0);
+        List<Group> groups = groupDao.searchGroupByName("group");
+        PageInfo<Group> groupPageInfo = new PageInfo<>(groups);
+        System.out.println("1. groupPageInfo: " + groupPageInfo);
+
+        groupPageInfo = PageHelper.startPage(1, 1).doSelectPageInfo(() -> groupDao.searchGroupByName("group"));
+        System.out.println("2. groupPageInfo: " + groupPageInfo);
+
+        Page<Group> groupPage = PageHelper.startPage(1, 2).doSelectPage(() -> groupDao.searchGroupByName("group"));
+        System.out.println("3. groupPage: " + groupPage);
+    }
 
     @Test
     public void testSearchGroupByName() {
